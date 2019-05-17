c76e48f000cea9bbc6afab76d96eb5e9854cfb1f
James F <jf423@hmgcc.gov.uk>
2019-05-17 10:38:30 +0100

    Testing

--- src_functions/test.js
+++ src_functions/test.js
@@ -1,15 +1,19 @@
 // src_functions/test.js
+//import net from "../brain/model"
+//import next from "../brain/next"
+
 exports.handler = function(event, context, callback) {
 
     // If you are building something serious you should probably 
     // validate that request type :D
-    if (event.httpMethod !== "POST") {
-      return { statusCode: 405, body: "Method Not Allowed" };
-    }
+    // if (event.httpMethod !== "POST") {
+    //   return { statusCode: 405, body: "Method Not Allowed" };
+    // }
 
-    const string = event.body
-    callback(null, {
-    statusCode: 200,
-    body: `Hello, ${string}`
-    });
+    // const string = event.body
+    // callback(null, {
+    // statusCode: 200,
+    // body: JSON.stringify(next(board, net))
+    // });
+    console.log(event);
 }
